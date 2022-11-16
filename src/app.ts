9f03abab46c8a0d1be04fcc8057ec0398fccb2a7
nicollyrocha <nicollyrcs@gmail.com>
2022-11-16 17:36:37 -0300

    att

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
-import * as express from 'express';
-import * as cors from 'cors';
-import * as logger from 'morgan';
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 import { router } from './routes/index';
+
+require('dotenv').config();
 
 export const app = express();
 
+const PORT = 8000;
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-app.use(logger('dev'));
-
 app.use('/', router);
+
+app.listen(PORT, () => {
+	console.log(`API started at port ${PORT}`);
+});
