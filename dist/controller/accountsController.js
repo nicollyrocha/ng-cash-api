654c10c25d52c31c89a210e16038bb7234b5ef4c
nicollyrocha <nicollyrcs@gmail.com>
2022-11-17 21:56:44 -0300

    revertendo commits

--- dist/controller/accountsController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AccountsController = void 0;
-class AccountsController {
-    async create() {
-        //criar conta
-    }
-}
-exports.AccountsController = AccountsController;
