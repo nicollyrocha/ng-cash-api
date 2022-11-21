9611e3d50ca265afe277e1885c06ee59ee5ded68
nicollyrocha <nicollyrcs@gmail.com>
2022-11-20 22:29:41 -0300

    att

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,20 @@ import AppError from './utils/appError';
 import authRouter from './routes/auth.routes';
 import userRouter from './routes/user.routes';
 import validateEnv from './utils/validateEnv';
-import redisClient from './utils/connectRedis';
+import cluster from 'cluster';
+import os from 'os';
 
+// import nodemailer from 'nodemailer';
+// (async function () {
+//   const credentials = await nodemailer.createTestAccount();
+//   console.log(credentials);
+// })();
+
+const numCpus = os.cpus().length;
 AppDataSource.initialize()
   .then(async () => {
     // VALIDATE ENV
+    validateEnv();
 
     const app = express();
 
@@ -25,15 +34,12 @@ AppDataSource.initialize()
 
     // 1. Body parser
     app.use(express.json({ limit: '10kb' }));
-    console.log("body parser")
+
     // 2. Logger
     if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
-    console.log("logger")
 
     // 3. Cookie Parser
     app.use(cookieParser());
-    console.log("cookie parser")
-
 
     // 4. Cors
     app.use(
@@ -42,52 +48,54 @@ AppDataSource.initialize()
         credentials: true,
       })
     );
-    console.log("cors")
 
     // ROUTES
     app.use('/api/auth', authRouter);
     app.use('/api/users', userRouter);
-    console.log("routes")
-
 
     // HEALTH CHECKER
     app.get('/api/healthChecker', async (_, res: Response) => {
-      const message = await redisClient.get('try');
+      // const message = await redisClient.get('try');
 
       res.status(200).json({
         status: 'success',
-        message,
+        message: 'Welcome to Node.js, we are happy to see you',
       });
     });
-    console.log("health checker")
-
 
     // UNHANDLED ROUTE
     app.all('*', (req: Request, res: Response, next: NextFunction) => {
       next(new AppError(404, `Route ${req.originalUrl} not found`));
     });
-    console.log("unhandled route")
-
 
     // GLOBAL ERROR HANDLER
     app.use(
       (error: AppError, req: Request, res: Response, next: NextFunction) => {
         error.status = error.status || 'error';
         error.statusCode = error.statusCode || 500;
-        console.log("error app.ts")
+
         res.status(error.statusCode).json({
           status: error.status,
           message: error.message,
         });
       }
     );
-    console.log("global error handler")
-
 
     const port = config.get<number>('port');
-    app.listen(port);
+    if (cluster.isPrimary) {
+      for (let i = 0; i < numCpus; i++) {
+        cluster.fork();
+      }
 
-    console.log(`Server started on port: ${port}`);
+      cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker pid: ${worker.process.pid} died`);
+        cluster.fork();
+      });
+    } else {
+      app.listen(port);
+      console.log(`Server started with pid: ${process.pid} on port: ${port}`);
+    }
+    // app.listen(port);
+    // console.log(`Server started with pid: ${process.pid} on port: ${port}`);
   })
-  .catch((error) => console.log("ERROR AQUI",error));
-
+  .catch((error) => console.log("Errorrrrrr",error));
--- src/utils/data-source.ts
+++ src/utils/data-source.ts
@@ -1,17 +1,22 @@
-require('dotenv').config()
+require('dotenv').config();
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
+import config from 'config';
 
-const port = process.env.POSTGRES_PORT as number | undefined
+const postgresConfig = config.get<{
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+}>('postgresConfig');
 
 export const AppDataSource = new DataSource({
+  ...postgresConfig,
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: 6500,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  entities: [`src/entities/*.{ts,js}`],
-	migrations: [`src/migrations/*.{ts,js}`],
+  synchronize: false,
+  logging: false,
+  entities: ['src/entities/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/**/*{.ts,.js}'],
+  subscribers: ['src/subscribers/**/*{.ts,.js}'],
 });
-
